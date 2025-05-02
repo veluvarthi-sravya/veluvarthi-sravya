953fc62162e481783292c47c2fa327a80dd7a75b
veluvarthi-sravya <sravyaveluvarthi07@gmail.com>
2025-05-02 11:26:56 +0530

    changed vite base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://veluvarthi-sravya.github.io/",
+  base:"/veluvarthi-sravya/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
